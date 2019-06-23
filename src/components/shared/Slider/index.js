7e314666964bcda7849986de646e70d45d01b07b
Vamshi Maddur <maddur.vamshi@gmail.com>
2019-06-23 17:27:45 -0400

    dev dep added

---
+++ src/components/shared/Slider/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import AwesomeSlider from 'react-awesome-slider';
+import 'react-awesome-slider/dist/styles.css';
+
+const Slider = (
+    <AwesomeSlider>
+      <div data-src="shared/images/1.jpg" />
+      <div data-src="shared/images/2.jpg" />
+      <div data-src="shared/images/3.jpg" />
+      <div data-src="shared/images/4.jpg" />
+    </AwesomeSlider>
+  );
+
+  export default Slider;
